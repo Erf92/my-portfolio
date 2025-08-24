246041e81e7bda56ceee9957fab8ca590b2187da
Erfan Rajaei <erf92raj@gmail.com>
2025-08-24 13:23:58 +0330

    fix contact section button and icons

--- src/components/ContactSection.tsx
+++ src/components/ContactSection.tsx
@@ -5,12 +5,23 @@ export default function ContactSection() {
     <section className="flex flex-col gap-4 items-start justify-center">
       <h3 className="text-xl font-bold">Contact</h3>
       <p>Lets build something amazing together!</p>
-      <button className="bg-blue-500 text-white px-2 py-1 rounded-xl">
+      <a
+        href="mailto:erf92raj@gmail.com"
+        className="bg-blue-600 text-white px-2 py-1 rounded-xl hover:bg-blue-800"
+      >
         Say Hello
-      </button>
+      </a>
       <div className="flex gap-3 ml-1 text-2xl">
-        <FaGithub />
-        <FaTelegram />
+        <a
+          href="https://github.com/Erf92"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FaGithub className="text-neutral-800 hover:text-black" />
+        </a>
+        <a href="https://t.me/erf_92" target="_blank" rel="noopener noreferrer">
+          <FaTelegram className="text-blue-500 hover:text-blue-700" />
+        </a>
       </div>
     </section>
   );
